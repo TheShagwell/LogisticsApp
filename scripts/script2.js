8ea616c7a463a72d8b41165594b256549616d9f3
manueldiamond <manueldiamondlistowell@gmail.com>
2023-07-18 16:43:38 +0100

    a

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,49 +1,52 @@
 // import { Chart } from "chart.js";
 
 const charts={}
+const chartsData={
+  statistics:chartData()
+}
 document.querySelectorAll('chart').forEach((chart) =>{
   chart.style.display="block";
   const canvas=document.createElement("canvas")
   chart.appendChild(canvas)
 
-  charts[chart.id]=new Chart(canvas, {type: chart.getAttribute("type"),chartData})
+  const labels=chart.getAttribute("labels")
+  const datasets= Array.from(chart.querySelectorAll("data")).map(dataset => (
+    {
+      ...Array.from(dataset.attributes).map(attriute=>(
+        {
+          [attriute.name]:attriute.value
+        }
+      ))
+    }
+    ))
+  
+  
+  charts[chart.id]=new Chart(canvas, chartConfig(
+    chart.getAttribute("type"),
+    chartData(
+      labels,
+      datasets
+    )
+  ))
 })
 
-// initCharts()
-// async function initCharts(){
-//   const data=await getAlphaData() 
-//   console.log(await getAlphaData())
-//   charts.keys().forEach((chartName)=>{
-//     switch(chartName){
-//       case "statistics":
-        
-//         break
-//       default:
-//         break
-//     }
-//   })
-
-// }
-
-function chartData(type,labels=["A","B","C"],){
-  return {
-    type,
-    data: {
-      labels,
-      datasets: [{
-        label: 'No data',
-        data: [],
-        borderWidth: 0,
-        backgroundColour:[
-
-        ]
-
-      }]
-    }
-  }
+async function randomAPIChartData(){
+  const timeSeriesData=await getAlphaData();
+}
+function chartConfig(type,data,options={}){
+  return {type,data,options}
+}
+function chartData(labels,datasets){
+  return { labels, datasets }
 }
 
-
+function dataset(label,data,extras={}){
+  return{
+    label,
+    data,
+    ...extras
+  }
+}
 
 function getAlphaData(){
   const alphaAPIurl = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=PTPK36I3GLZ99QC5';
@@ -55,5 +58,5 @@ function getAlphaData(){
     .catch(err=>{
       console.log("There is an Error: ",err)
     })
-}
+   
 
